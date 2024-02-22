ee637cf057c68d76f05e06ee2189f5f602a7f7f1
Nikesh Saini <nikeshsplash@gmail.com>
2024-02-23 05:02:52 +0530

    structure modification

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -3,7 +3,9 @@ import "./body.css"
 export default function Body(){
     return (
         <div className="body">
-
+                <div className="main-content">
+                    <h1>ddddddddd</h1>
+                </div>
         </div>
     );
 };
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,35 @@ import  "./footer.css";
 export default function Footer(){
     return (
         <div className="footer">
-        <h1>footer</h1>
+            <div className="left-part">
+                <div className="left-part-section1">
+                    <ul>
+                        <p>CATEGORIES</p>
+                        <li>WebSite Builder</li>
+                        <li>Hosting</li>
+                        <li>Data Center </li>
+                        <li>Robotic-Automation</li>
+                    </ul>
+                </div>
+                <div className="left-part-section2">
+                    <ul>
+                        <p>CONTACT</p>
+                        <li> Contact</li>
+                        <li>Priacy Policy</li>
+                        <li>Terms of Service </li>
+                        <li>Categories</li>
+                        <li>About</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="right-part">
+            <select className='dropdown'>
+                <option value="link0" selected >United States</option>
+                <option value="link1">Link 1</option>
+                <option value="link2">Link 2</option>
+                <option value="link3">Link 3</option>
+</select>
+            </div>
         </div>
     );
 };
